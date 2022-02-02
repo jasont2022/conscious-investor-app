72117776299e9f5eec0acb6e5f34b3850f797f04
Anirudh Agrawal <anirudhagrawal09@gmail.com>
2022-02-02 15:46:44 -0500

    Sidebar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './App.scss';
 // pages for this product
 import Home from './views/Home';
 import Error from './views/Error';
 import Register from './views/Register';
 import Login from './views/Login';
+import AboutUs from './views/AboutUs';
+//import AppLayout from './views/Layout/AppLayout.js';
 
 render(
   <Router>
@@ -15,6 +17,8 @@ render(
       <Route path='/' element={<Home />} />
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/aboutus' element={<AboutUs />} />
+      
       <Route path='*' element={<Error />} />
     </Routes>
   </Router>,
