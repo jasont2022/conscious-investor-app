08380e688a4b7f8a9aa0c94b6875fdb92ac3b0c2
kashkaryan <kachik@seas.upenn.edu>
2022-03-27 13:24:59 -0400

    All Done Boys

--- frontend/src/views/DashHome.js
+++ frontend/src/views/DashHome.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
+import { useParams } from 'react-router-dom';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
 import FormGroup from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+// import Select from '@mui/material/Select';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import axios from 'axios';
@@ -12,6 +13,18 @@ import { DataGrid } from '@mui/x-data-grid';
 import Avatar from '@mui/material/Avatar';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Navbar from '../NavNew/TopNav';
+
+import MuiGrid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material'
+var htmlparser = require('htmlparser2');
+
+
+
 const columns = [
   { field: 'id', headerName: 'Number', width: 90 },
   {
@@ -44,10 +57,14 @@ const columns = [
     width: 200,
     editable: true,
   }
+  
 ];
 
+
+
+
 const financial_columns = [
-  { field: 'id', headerName: 'Ticker', width: 90 },
+  { field: 'id', headerName: 'Ticker', width: 60 },
   {
     field: 'logo',
     headerName: 'Logo',
@@ -62,7 +79,7 @@ const financial_columns = [
   {
     field: 'companyName',
     headerName: 'Company name',
-    width: 300,
+    width: 150,
     editable: true,
   },
   {
@@ -85,101 +102,167 @@ const financial_columns = [
   }
 ];
 
+const News = ({ article }) => {
+  const { author, text, publishedDate, title, url, image } = article || {}
+  var result = [];
 
-export default class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      industry: 'Information Technology',
-      state:'',
-      dividend:false,
-      model:'esg',
-      rows : 10,
-      full_recs: []
+  var parser = new htmlparser.Parser({
+    ontext: function (text) {
+      result.push(text);
     }
+  }, { decodeEntities: true });
 
-    this.handleChangeRows = this.handleChangeRows.bind(this);
-    this.handleApplyChanges = this.handleApplyChanges.bind(this);
-  }
-  handleChangeRows(event) {
-    this.setState({rows: event.target.value});
-    event.preventDefault();
-  }
-  
-  handleChangeModel(event) {
-    this.setState({model: event.target.value});
-    event.preventDefault();
-  }
+  parser.write(text);
+  parser.end();
 
-  handleApplyChanges(event) {
-    this.setState({ full_recs : []})
-    if (this.state.model === 'esg'){
-      axios.get(`/account/portfolio/`).then(res => {
-        var total = res.data
-        
-        total.forEach(total => {
-          
-          axios.get(`https://finnhub.io/api/v1/stock/profile2?symbol=${total}&token=c80soqqad3ie5egte6d0`).then(comp => {
-            axios.get(`/account/ticker/${total}`).then(scr => {
-              var score = scr
-              var thenum = score.data.replace( /^\D+/g, '');
-              var recsCopy = this.state.full_recs.slice()
-              var single = {
-                id: total[0],
+  result.join('');
+
+  return (
+    <div>
+      <Card sx={{ maxWidth: "80%", marginTop: "40px", marginLeft: "5%"}}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={image}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              {title}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              {author}
+            </Typography>
+            <Typography gutterBottom variant="h8" component="div">
+              {publishedDate.slice(0, 10)}
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              {result}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary" href={url} target="_blank">
+            Go To Article
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+const Grid = styled(MuiGrid)(({ theme }) => ({
+  width: '100%',
+  ...theme.typography.body2,
+  '& [role="separator"]': {
+    margin: theme.spacing(0, 2),
+  },
+}));
+
+
+const Dashboard = () => {
+
+    const [fullRecs, setFullRecs] = useState([]); // full_recs
+    const { tick } = useParams()
+    const [articlesAll, setArticles] = useState([]) // get the news artcles
+
+    const model = 'esg';
+    const rows = 10;
+
+    useEffect(() => {
+        handleApplyChanges()
+    }, []);
+
+
+
+  const handleApplyChanges = async () => {
+
+
+    const { data } = await axios.get(`https://financialmodelingprep.com/api/v3/stock_news?limit=5&apikey=e605026ed16aae3b084c6297f09bba6c`)
+    setArticles(data)
+
+    if (model === 'esg'){
+
+        axios.get(`/account/portfolio/`).then(res => {
+          console.log('res', res.data)
+          const recsCopy = []
+          res.data.forEach(code => {
+            axios.get(`https://finnhub.io/api/v1/stock/profile2?symbol=${code}&token=c80soqqad3ie5egte6d0`).then(comp => {
+            axios.get(`/account/ticker/${code}`).then(score => {
+              const scoreString = " " + (score.data.score)
+              const thenum = scoreString.replace( /^\D+/g, '');
+              
+              const single = {
+                id: code,
                 logo: comp.data.logo,
                 companyName: comp.data.name,
                 industry: comp.data.finnhubIndustry,
                 esg: Math.floor(Number(thenum * 1000))/10
-
               }
+              console.log(single)
               recsCopy.push(single)
-              this.setState({ full_recs : recsCopy})
+              console.log(recsCopy)
+              if (recsCopy.length === res.data.length) {
+                setFullRecs(recsCopy)
+            }              
             })
           })
         });
+        
       })
+      
     } 
-    event.preventDefault();
   }
 
-  render(){
   return (
     <div>
-      <style>{'body {background-color: white; padding: 0px 0px 0px 323px }'}</style>
-      <Navbar/>
+      <style>{'body {background-color: white; padding: 20px 0px 0px 323px }'}</style>
       <Sidebar/>
       <Container maxWidth={false} sx={{ backgroundColor: '#EFE5FF'}}>
         <Box sx={{ fontWeight: 'bold', fontSize: 40, textAlignLast: 'center'}}>Dashboard</Box>
         <Box sx={{ fontWeight: 'bold', height: 10 }}></Box>
       </Container>
-      <FormGroup className="MuiFormGroup-options" row sx={{display:"block", textAlignLast:'center'}}>
-   
-      
-      
-      <Button
-        size="small"
-        variant="outlined"
-        color="primary"
-        sx={{width:100, margin:5}}
-        onClick={this.handleApplyChanges}
-      >
-        Refresh
-      </Button>
+
+      <FormGroup className="MuiFormGroup-options" sx={{display:"block", textAlignLast:'center'}}>
+        <Button
+            size="small"
+            variant="outlined"
+            color="primary"
+            sx={{width:100, margin:5}}
+            onClick={handleApplyChanges}
+        >
+            Refresh
+        </Button>
       </FormGroup>
 
 
       <div style={{ height: 600, width: '70%', margin: 'auto'}}>
-      <DataGrid
-        rows={this.state.full_recs.sort((a, b) => parseFloat(b.excess_return) - parseFloat(a.excess_return)).slice(0, this.state.rows)}
-        columns={this.state.model === 'esg' ? columns : financial_columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        checkboxSelection
-        disableSelectionOnClick
-      />
-    </div>
+        <DataGrid
+            rows={fullRecs.sort((a, b) => parseFloat(b.excess_return) - parseFloat(a.excess_return)).slice(0, rows)}
+            columns={model === 'esg' ? columns : financial_columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+            checkboxSelection
+            disableSelectionOnClick
+        />
+      </div>
+      <br/>
+      <br/>
 
+      <Typography variant="h4" gutterBottom component="div" style={{marginLeft: "5%"}}>
+          News
+        </Typography>
+
+        {console.log(articlesAll)}
+      
+        <div style={{ marginBottom: '50px' }}>
+          {articlesAll.map((article, i) => <News key={i} article={article} />)}
+        </div>
+
+      
     </div>
   );
-  }
-}
+};
+
+export default Dashboard; 
